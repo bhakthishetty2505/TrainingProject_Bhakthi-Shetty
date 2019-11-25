a956ec36968835a77dc7ef03828b64b532d9c540
bhakthishetty2505 <bhakthishetty1697@gmail.com>
2019-11-25 13:38:19 +0530

    Java-JDBC Assessment

---
+++ contact_file/appdevelopment/src/com/tyss/appdevelopment/DTO/ContactBean.java
@@ -0,0 +1,30 @@
+package com.tyss.appdevelopment.DTO;
+
+public class ContactBean {
+
+	private String name;
+	private int number;
+	private String contactGroup;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getContactGroup() {
+		return contactGroup;
+	}
+	public void setContactGroup(String contactGroup) {
+		this.contactGroup = contactGroup;
+	}
+	
+	
+}
